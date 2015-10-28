bfa87b45e77ad5763fbdc95b18d4f999ae1626b1
Maciej Mensfeld <maciej@mensfeld.pl>
2015-10-28 17:16:18 +0200

    Wed Oct 28 17:16:18 EET 2015 touch

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-# #!/bin/bash
+set -e
 
 PATH=/volume1/homes/admin/data/github/contributor
 
@@ -9,6 +9,6 @@ DATE=`/bin/date`
 /bin/echo $DATE >> ./counter
 
 /opt/bin/git add --all ./
-/opt/bin/git commit -m "$DATE touch"
-/opt/bin/git push origin master
+/opt/bin/git commit -m "$DATE touch" > ./everything.txt 2>&1
+/opt/bin/git push origin master > ./everything.txt 2>&1
 
